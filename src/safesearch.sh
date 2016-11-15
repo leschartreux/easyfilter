8e210336a7be0e2df2e4d25da7125fa0d885dd21
Raphaël <raphael@rignier.com>
2016-11-15 15:37:41 +0100

    Update safesearch.sh

--- src/safesearch.sh
+++ src/safesearch.sh
@@ -1,7 +1,26 @@
+wget https://www.google.com/supported_domains
+
 echo ";safesearch for google" > safesearch.conf
-for i in `cat googlecountry`
+for i in `cat supported_domains`
 do
 	g=`echo $i | sed 's/^.//'`
 	echo $g CNAME forcesafesearch.google.com. >> safesearch.conf;
 	echo "*.$g" CNAME forcesafesearch.google.com. >> safesearch.conf 
 done
+
+echo >> safesearch.conf
+echo ";safe search for Youtube" >> safesearch.conf
+for i in `cat youtube_domains`
+do
+    echo $i CNAME restrictmoderate.youtube.com. >> safesearch.conf
+done
+
+echo >> safesearch.conf
+echo ";safe search for Qwant" >> safesearch.conf
+echo "api.qwant.com. CNAME safeapi.qwant.com." >> safesearch.conf
+
+echo >> safesearch.conf
+echo ";safesearch for Bing" >> safesearch.conf
+echo "www.bin.com. CNAME strict.bing.com." >> safesearch.conf
+
+    
